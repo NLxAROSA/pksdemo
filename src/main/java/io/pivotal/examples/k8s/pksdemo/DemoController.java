b25e0fc67321eb2111ccd1b4b27bd5b36a158bf1
Lars Rosenquist <lrosenquist@pivotal.io>
2019-11-18 14:57:06 +0100

    Added Azure File storage example

--- src/main/java/io/pivotal/examples/k8s/pksdemo/DemoController.java
+++ src/main/java/io/pivotal/examples/k8s/pksdemo/DemoController.java
@@ -1,5 +1,9 @@
 package io.pivotal.examples.k8s.pksdemo;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -12,10 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-    @GetMapping
+    @GetMapping("/")
     public String helloPks() {
         return "Hello PKS! The current time is: "
                 + new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX").format(new Date()) + ", have a great day today!";
     }
 
+    @GetMapping("/filez")
+    public String getFile() throws Exception {
+        File f = new File("/mnt/azure/test.txt");
+        if (f.exists()) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+            String s = br.readLine();
+            return s;
+        } else {
+            return "Can't find file..";
+        }
+    }
 }
